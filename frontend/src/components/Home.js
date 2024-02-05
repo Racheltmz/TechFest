da730f9f801b0588f80cb8b47fad90958efd3907
Rachel Tan <racheltanmz@gmail.com>
2024-02-05 17:15:17 +0800

    Updated output features

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,6 +3,7 @@ import { Button, Row } from 'react-bootstrap';
 import { MdCloudUpload, MdDelete } from 'react-icons/md';
 import { AiFillFileImage } from 'react-icons/ai';
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { LuListMusic } from "react-icons/lu";
 import Swal from 'sweetalert2';
 import { styleTransfer } from '../service/MusicService';
 import '../styles/Utilities.css';
@@ -103,7 +104,7 @@ export default function Home() {
                                 onChange={handleFileInputChange} // Handle file input change
                             />
                             {audio ?
-                                <audio controls autoPlay><source src={audio} type="audio/mpeg"></source></audio>
+                                <audio controls><source src={audio} type="audio/mpeg"></source></audio>
                                 :
                                 <>
                                     <MdCloudUpload color='#1475cf' size={60} />
@@ -124,17 +125,16 @@ export default function Home() {
                             </span>
                         </div>
                     </div>
-
                     <div className='form-content' id='reference'>
                         <h2>2. Select a Genre:</h2>
-                        <Button variant="primary" className='genre' type='button' value='jazz' onClick={handleGenre}>Jazz</Button>
-                        <Button variant="primary" className='genre' type='button' value='rock' onClick={handleGenre}>Rock</Button>
-                        <Button variant="primary" className='genre' type='button' value='disco' onClick={handleGenre}>Disco</Button>
-                        <Button variant="primary" className='genre' type='button' value='classical' onClick={handleGenre}>Classical</Button>
-                        <Button variant="primary" className='genre' type='button' value='country' onClick={handleGenre}>Country</Button>
-                        <Button variant="primary" className='genre' type='button' value='hiphop' onClick={handleGenre}>Hiphop</Button>
-                        <Button variant="primary" className='genre' type='button' value='metal' onClick={handleGenre}>Metal</Button>
-                        <Button variant="primary" className='genre' type='button' value='pop' onClick={handleGenre}>Pop</Button>
+                        <Button variant="primary" className={genre === 'jazz' ? 'active genre' : 'genre'} type='button' value='jazz' onClick={handleGenre}>Jazz</Button>
+                        <Button variant="primary" className={genre === 'rock' ? 'active genre' : 'genre'} type='button' value='rock' onClick={handleGenre}>Rock</Button>
+                        <Button variant="primary" className={genre === 'disco' ? 'active genre' : 'genre'} type='button' value='disco' onClick={handleGenre}>Disco</Button>
+                        <Button variant="primary" className={genre === 'classical' ? 'active genre' : 'genre'} type='button' value='classical' onClick={handleGenre}>Classical</Button>
+                        <Button variant="primary" className={genre === 'country' ? 'active genre' : 'genre'} type='button' value='country' onClick={handleGenre}>Country</Button>
+                        <Button variant="primary" className={genre === 'hiphop' ? 'active genre' : 'genre'} type='button' value='hiphop' onClick={handleGenre}>Hiphop</Button>
+                        <Button variant="primary" className={genre === 'metal' ? 'active genre' : 'genre'} type='button' value='metal' onClick={handleGenre}>Metal</Button>
+                        <Button variant="primary" className={genre === 'pop' ? 'active genre' : 'genre'} type='button' value='pop' onClick={handleGenre}>Pop</Button>
                     </div>
                     <div id='convert'>
                         <Button variant="primary" type='submit' onClick={handleSubmit}>Convert Audio<span id='convert_icon'><FaRegArrowAltCircleRight /></span></Button>
@@ -143,15 +143,19 @@ export default function Home() {
                 <Row className='col-md-6' id='generated'>
                     <div id='content_area'>
                         <h2>Output</h2>
-                        {genAudio ?
-                            <audio controls autoPlay><source src={require(`../videos/${genAudio}`)} type="audio/mpeg"></source></audio>
+                        {genImage && genAudio ?
+                            <div>
+                                <div id='genImg'><img src={require(`../images/${genImage}`)} alt='generated' width="500" height="500"></img></div>
+                                <div id='genAudio'>
+                                    <hr></hr>
+                                    <h2>{fileName.replace(".wav", "")} in {genre}</h2>
+                                    <audio controls><source src={require(`../videos/${genAudio}`)} type="audio/mpeg"></source></audio>
+                                </div>
+                            </div>
                             :
-                            <div></div>
-                        }
-                        {genImage ?
-                            <img src={require(`../images/${genImage}`)} alt='generated' width="100" height="100"></img>
-                            :
-                            <div></div>
+                            <div>
+                                <LuListMusic id='orig_icon' />
+                            </div>
                         }
                     </div>
                 </Row>
